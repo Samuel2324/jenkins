d87e1cb240b56041912610768c2fb0760cf50db8
Samuel2324 <mca.2324@unigoa.ac.in>
2024-10-24 12:54:22 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -3,6 +3,6 @@ class Helloworld {
     // Prints "Hello, World" to the terminal window.
     public static void main(String args[])
     {
-        System.out.println("Hello, World 1");
+        System.out.println("Hello, World 2");
     }
 }
